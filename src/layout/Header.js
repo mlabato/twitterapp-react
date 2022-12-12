ab555a45d022f2cf4756b9372a88e830eab16853
mlabato <m_labato@hotmail.com>
2022-12-12 15:12:45 -0300

    mobile navbar fixed

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -18,7 +18,7 @@ const Header = () => {
         <img src="/logo.png" className="hidden notebook:block my-2 notebook:my-4 h-[1.5rem] notebook:h-[2rem] ml-[1rem] notebook:ml-[3rem] " alt="" />
 
         <HiMenu className="text-[#1C94E5] text-[1.75rem] notebook:hidden" onClick={showBarHandler}/>
-        <MobileNav classes={showBar ? " left-0 top-10" : "top-10 -left-[30rem]"}/>
+        <MobileNav classes={showBar ? " left-0 top-10" : "top-10 -left-[50rem]"}/>
         
 
         <div className="text-gray-500 flex flex-row items-center text-[0.7rem] notebook:text-[1rem]">
